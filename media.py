bba882e6c0b61c816b39e173c5fad9a15962dede
Luciano <134172776+RukaRey@users.noreply.github.com>
2023-09-08 17:10:25 -0300

    Add files via upload

---
+++ media.py
@@ -0,0 +1,6 @@
+num = [0,1,2,3,4]
+print("Calcule a média entre 5 números inteiros.")
+for i in range(5):
+    num[i]=int(input("Num:"))
+media=((num[0]+num[1]+num[2]+num[3]+num[4])/(i+1))
+print("A média numérica entre os números é ",media,".")
